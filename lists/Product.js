27307c5860475e6fe8f728e2a033878b65481e8f
Seth Davis <sethdavis512@gmail.com>
2020-07-18 12:53:52 -0500

    Add cloudinary image upload to keystone product list

--- lists/Product.js
+++ lists/Product.js
@@ -1,7 +1,20 @@
-const { Decimal, Text } = require('@keystonejs/fields');
+const { CloudinaryAdapter } = require('@keystonejs/file-adapters');
+const { CloudinaryImage, Decimal, Text } = require('@keystonejs/fields');
+require('dotenv').config();
+
+const adapter = new CloudinaryAdapter({
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    apiKey: process.env.CLOUDINARY_KEY,
+    apiSecret: process.env.CLOUDINARY_SECRET,
+    folder: process.env.CLOUDINARY_DIR_NAME
+});
 
 module.exports = {
     fields: {
+        image: {
+            type: CloudinaryImage,
+            adapter
+        },
         title: {
             type: Text,
             isRequired: true
