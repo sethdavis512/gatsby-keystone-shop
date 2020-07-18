1a021650e2d55cdb1235dfd20d4c3ac8843f6ef7
Seth Davis <sethdavis512@gmail.com>
2020-07-18 12:51:53 -0500

    Add get product by id query

--- client/src/queries/Product.ts
+++ client/src/queries/Product.ts
@@ -1,8 +1,24 @@
 export const GET_ALL_PRODUCTS = `
 query GetProducts {
-  allProducts {
-    title,
-    description,
-    price
-  }
+    allProducts {
+        id
+        title
+        description
+        image {
+            publicUrl
+        }
+        price
+    }
+}`
+
+export const GET_PRODUCT = `
+query GetProduct($id: ID!) {
+    Product(where: { id: $id }) {
+        title
+        description
+        image {
+            publicUrl
+        }
+        price
+    }
 }`
