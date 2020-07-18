28be5f5879a56a35d6cce2524e5b4aaa0a3af60f
Seth Davis <sethdavis512@gmail.com>
2020-07-18 12:51:21 -0500

    Move products to shop page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,45 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-import Card from '../components/Card'
 import Columns from '../components/Columns'
 import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 import Wrapper from '../components/Wrapper'
 
-import { GET_ALL_PRODUCTS } from '../queries/Product'
-
 const IndexPage = () => {
-    const [stickers, setStickers] = useState([])
-
-    useEffect(() => {
-        const fetchStickers = async () => {
-            const results = await axios.post('/admin/api', {
-                query: GET_ALL_PRODUCTS
-            })
-            setStickers(results.data.data.allProducts)
-        }
-
-        fetchStickers()
-    }, [])
-
-    const mappedCards =
-        stickers &&
-        stickers.map(sticker => (
-            <Card>
-                <Card.Image />
-                <Card.Content
-                    title={sticker.title}
-                    description={sticker.description}
-                />
-            </Card>
-        ))
-
-    const mappedColumns = mappedCards.map(card => (
-        <Columns.Column className="is-3">{card}</Columns.Column>
-    ))
-
     return (
         <Layout>
             <SEO title="Home" />
@@ -47,7 +14,11 @@ const IndexPage = () => {
                 <h1 className="title">Welcome</h1>
             </Hero>
             <Wrapper>
-                <Columns className="is-multiline">{mappedColumns}</Columns>
+                <Columns>
+                    <Columns.Column>
+                        <p>Thanks for visiting!</p>
+                    </Columns.Column>
+                </Columns>
             </Wrapper>
         </Layout>
     )
